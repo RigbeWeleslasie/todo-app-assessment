4cfcbe4d01e5cc878bf82d701caceb6ee40a6f9a
RigbeWeleslasie <rigbewweleslasie@gmail.com>
2026-02-25 13:53:55 +0300

    feat: send device token with every API request

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
+const getDeviceToken = () => {
+    let token = localStorage.getItem('device_token');
+    if (!token) {
+        token = 'dt_' + Math.random().toString(36).substr(2, 9) + '_' + Date.now();
+        localStorage.setItem('device_token', token);
+    }
+    return token;
+};
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'X-Device-Token': getDeviceToken(),
     }
 });
 
